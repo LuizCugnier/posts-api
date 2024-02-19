ad29551ac4ba7ca459ef9934b6bc4f1bcac4ee7d
GutoCugnier <luizcugnier.neto@gmail.com>
2024-02-19 06:38:14 -0300

    Add CreatePost service to PostsModule

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { PostsController } from './posts.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Post } from './entities/post.entity';
+import { CreatePost } from './use-cases/create-post.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Post])],
-  controllers: [PostsController]
+  controllers: [PostsController],
+  providers: [CreatePost],
 })
 export class PostsModule {}
