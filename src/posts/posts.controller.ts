3f25d3e630ee089a5d011297b04ba43ae32775fc
GutoCugnier <luizcugnier.neto@gmail.com>
2024-02-19 06:38:20 -0300

    Add create post functionality to PostsController

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreatePost } from './use-cases/create-post.service';
+import { CreatePostDto } from './dto/create-post.dto';
 
 @Controller('posts')
-export class PostsController {}
+export class PostsController {
+    constructor(
+        private readonly createPost: CreatePost
+    ) {}
+
+    @Post()
+    create(@Body() input: CreatePostDto) {
+        return this.createPost.create(input);
+    }
+}
