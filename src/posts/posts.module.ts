92f7edc145ee9cb8dc6456650b94ee2ad32b6ad7
GutoCugnier <luizcugnier.neto@gmail.com>
2024-02-22 14:21:41 -0300

    Refactor PostsModule imports and providers

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { PostsController } from './posts.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Post } from './entities/post.entity';
+import { Posts } from './entities/posts.entity';
 import { CreatePost } from './use-cases/create-post.service';
+import { FindAllPosts } from './use-cases/find-all-posts.service';
+import { FindPostById } from './use-cases/find-post-by-id.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Post])],
+  imports: [TypeOrmModule.forFeature([Posts])],
   controllers: [PostsController],
-  providers: [CreatePost],
+  providers: [CreatePost, FindAllPosts, FindPostById],
 })
 export class PostsModule {}
