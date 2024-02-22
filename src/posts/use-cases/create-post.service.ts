c299f25d8863e796e80cd2b137e8b6a9b8359138
GutoCugnier <luizcugnier.neto@gmail.com>
2024-02-22 14:22:24 -0300

    Refactor CreatePost service to use Posts entity

--- src/posts/use-cases/create-post.service.ts
+++ src/posts/use-cases/create-post.service.ts
@@ -1,19 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { Post } from '../entities/post.entity';
+import { Posts } from '../entities/posts.entity';
 import { CreatePostDto } from '../dto/create-post.dto';
 
 @Injectable()
 export class CreatePost {
   constructor(
-    @InjectRepository(Post)
-    private readonly postRepository: Repository<Post>,
+    @InjectRepository(Posts)
+    private readonly postRepository: Repository<Posts>,
   ) {}
-  
+
   create(input: CreatePostDto) {
     try {
-      const post = new Post(input);
+      const post = new Posts(input);
       return this.postRepository.save(post);
     } catch (error) {
       console.error('Erro ao criar o post', error);
