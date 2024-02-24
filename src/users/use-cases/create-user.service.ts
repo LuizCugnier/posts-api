ce2d3b0a84c37e51d53f4687ef68297d673b1a5a
Guto Cugnier <luizcugnier.neto@gmail.com>
2024-02-24 11:08:33 -0300

    add: password hash encrypt

--- src/users/use-cases/create-user.service.ts
+++ src/users/use-cases/create-user.service.ts
@@ -3,18 +3,27 @@ import { CreateUserDto } from '../dto/create-user.dto';
 import { User } from '../entities/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class CreateUser {
   constructor(
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
-  ) {}
-  
-  create(createUserDto: CreateUserDto) {
+  ) { }
+
+  async create(createUserDto: CreateUserDto) {
     try {
-      const user = new User(createUserDto);
-      return this.userRepository.save(user);
+
+      const user = {
+        ...createUserDto,
+        password: await bcrypt.hash(createUserDto.password, 10)
+      }
+
+      const createdUser = await this.userRepository.save(new User(user));
+
+      return {...createdUser, password: undefined};
+
     } catch (error) {
       console.error('Erro ao criar usuario', error);
       throw new Error('Usuario já cadastrado!');
